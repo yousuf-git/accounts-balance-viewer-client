613a731fef5842c45b82cb1bca5a171b4ab48da4
yousuf <mohammed.yousuf@pickme.lk>
2022-08-23 07:50:50 +0530

    set auth token in common interceptor

--- src/app/core/interceptors/common.interceptor.ts
+++ src/app/core/interceptors/common.interceptor.ts
@@ -2,16 +2,22 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from "../../../environments/environment";
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable()
 export class CommonInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(private cookieService: CookieService) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let token = this.cookieService.get('Authorization');
+
     const newRequest = request.clone({
-      url: environment.serverHost + request.url
+      url: environment.serverHost + request.url,
+      setHeaders: {
+        'Authorization': token ? `Bearer ${token}` : ''
+      }
     });
 
     return next.handle(newRequest);
