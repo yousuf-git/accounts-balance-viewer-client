4a69783add55372dbdbba7fe1b47026732d55384
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 06:33:40 +0530

    auth and global styling

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,11 @@ import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from "./auth-routing.module";
 import {AuthComponent} from "./auth.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCommonModule} from "@angular/material/core";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
@@ -10,7 +15,12 @@ import {ReactiveFormsModule} from "@angular/forms";
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCommonModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class AuthModule {
