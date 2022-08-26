d22e3d0187d15c7c00c0ffadb3c6e43012b67397
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 07:17:23 +0530

    refactor view balance component

--- src/app/modules/view-balance/view-balance.component.ts
+++ src/app/modules/view-balance/view-balance.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {CoreService} from "../../core/services/core.service";
 import {Account} from "../../core/models/entities/account";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {catchError, Observable, throwError} from "rxjs";
+import {GetAccountsResponse} from "../../core/models/responses/get-accounts-response";
 
 @Component({
   selector: 'app-view-balance',
@@ -12,11 +15,22 @@ export class ViewBalanceComponent implements OnInit {
   public accounts: Account[] = [];
   public todayDate: Date = new Date();
 
-  constructor(private coreService: CoreService) {
+  constructor(private _coreService: CoreService, private _snackbar: MatSnackBar) {
   }
 
   ngOnInit(): void {
-    this.coreService.getAccounts().subscribe(value => this.accounts = value);
+    this.fetchAccounts().subscribe(this.setAccounts);
+  }
+
+  public fetchAccounts(): Observable<GetAccountsResponse> {
+    return this._coreService.getAccounts().pipe(catchError(err => {
+      this._snackbar.open('Something went wrong while fetching accounts');
+      return throwError(err);
+    }));
+  }
+
+  public setAccounts(accounts: Account[]) {
+    this.accounts = accounts;
   }
 
 }
