dc0eda9a7ed6ff5ffe58bb89e37e8f77d909a807
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 07:17:46 +0530

    add unit tests for view balance component

--- src/app/modules/view-balance/view-balance.component.spec.ts
+++ src/app/modules/view-balance/view-balance.component.spec.ts
@@ -1,23 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ViewBalanceComponent} from './view-balance.component';
+import {CoreService} from "../../core/services/core.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {of} from "rxjs";
+import {GetAccountsResponse} from "../../core/models/responses/get-accounts-response";
+import {Account} from "../../core/models/entities/account";
 
-import { ViewBalanceComponent } from './view-balance.component';
+const getTestAccounts = (): Account[] => [
+  {id: 10, name: 'R&D', balance: 100_000.50},
+  {id: 14, name: 'Cash', balance: 250_000.25},
+  {id: 24, name: 'Operations', balance: 50_000.00}
+];
 
 describe('ViewBalanceComponent', () => {
+  let fakeCoreService: jasmine.SpyObj<CoreService>;
+  let fakeMatSnackBar: jasmine.SpyObj<MatSnackBar>;
   let component: ViewBalanceComponent;
-  let fixture: ComponentFixture<ViewBalanceComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ViewBalanceComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ViewBalanceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    fakeCoreService = jasmine.createSpyObj<CoreService>('CoreService', {
+      getAccounts: of([]),
+      addEntries: of({})
+    });
+    fakeMatSnackBar = jasmine.createSpyObj<MatSnackBar>('MatSnackBar', {
+      open: undefined
+    });
+    component = new ViewBalanceComponent(fakeCoreService, fakeMatSnackBar);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('fetches accounts', () => {
+    const accounts: GetAccountsResponse = getTestAccounts();
+
+    fakeCoreService.getAccounts.and.returnValue(of(accounts));
+
+    // act
+    const result = component.fetchAccounts();
+
+    // assert
+    result.subscribe(value => expect(value).toEqual(accounts));
   });
+
+  it('invokes core service to fetch accounts', () => {
+    const accounts: GetAccountsResponse = getTestAccounts();
+
+    fakeCoreService.getAccounts.and.returnValue(of(accounts));
+
+    // act
+    const result = component.fetchAccounts();
+
+    // assert
+    expect(fakeCoreService.getAccounts.calls.count()).toBe(1);
+  });
+
+  it('sets accounts', () => {
+    const accounts: GetAccountsResponse = getTestAccounts();
+
+    // act
+    component.setAccounts(accounts);
+
+    // assert
+    expect(component.accounts).toEqual(accounts);
+  });
+
 });
