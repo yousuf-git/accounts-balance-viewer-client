b7866a64a332748c2fcdfe3604033741532b1b13
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 06:28:02 +0530

    implement view balance feature

--- src/app/modules/view-balance/view-balance.component.ts
+++ src/app/modules/view-balance/view-balance.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CoreService} from "../../core/services/core.service";
+import {Account} from "../../core/models/entities/account";
 
 @Component({
   selector: 'app-view-balance',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBalanceComponent implements OnInit {
 
-  constructor() { }
+  public accounts: Account[] = [];
+  public todayDate: Date = new Date();
+
+  constructor(private coreService: CoreService) {
+  }
 
   ngOnInit(): void {
+    this.coreService.getAccounts().subscribe(value => this.accounts = value);
   }
 
 }
--- src/app/modules/view-balance/view-balance.module.ts
+++ src/app/modules/view-balance/view-balance.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ViewBalanceComponent} from "./view-balance.component";
 import {ViewBalanceRoutingModule} from "./view-balance-routing.module";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [ViewBalanceComponent],
   imports: [
     CommonModule,
-    ViewBalanceRoutingModule
+    ViewBalanceRoutingModule,
+    MatCardModule
   ]
 })
 export class ViewBalanceModule {
