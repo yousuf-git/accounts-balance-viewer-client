4d10d7494217919ccedbd7224551621cb1612097
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 08:42:36 +0530

    add unit tests for common interceptor

--- src/app/core/interceptors/common.interceptor.spec.ts
+++ src/app/core/interceptors/common.interceptor.spec.ts
@@ -1,16 +1,49 @@
-import {TestBed} from '@angular/core/testing';
-
 import {CommonInterceptor} from './common.interceptor';
+import {CookieService} from "ngx-cookie-service";
+import {HttpHandler, HttpRequest} from "@angular/common/http";
+import {of} from "rxjs";
+import {environment} from "../../../environments/environment.prod";
 
 describe('CommonInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      CommonInterceptor
-    ]
-  }));
+  let fakeCookieService: jasmine.SpyObj<CookieService>;
+  let fakeHttpHandler: jasmine.SpyObj<HttpHandler>;
+  let interceptor: CommonInterceptor;
 
-  it('should be created', () => {
-    const interceptor: CommonInterceptor = TestBed.inject(CommonInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    fakeCookieService = jasmine.createSpyObj<CookieService>('CookieService', {
+      get: undefined
+    });
+
+    fakeHttpHandler = jasmine.createSpyObj<HttpHandler>('HttpHandler', {
+      handle: of()
+    });
+
+    interceptor = new CommonInterceptor(fakeCookieService);
+  });
+
+  it('injects server host and auth token to the request', () => {
+    const request: HttpRequest<any> = new HttpRequest<any>('GET', 'example.com');
+    fakeCookieService.get.and.returnValue('5c44sd8321sd');
+
+    // act
+    interceptor.intercept(request, fakeHttpHandler)
+
+    // assert
+    const newRequest = fakeHttpHandler.handle.calls.argsFor(0)[0];
+    expect(newRequest.url).toContain(environment.serverHost);
+    expect(newRequest.headers.get('Authorization')).toBeTruthy();
+  });
+
+  it('injects only server host to the request and omits auth token when unauthenticated', () => {
+    const request: HttpRequest<any> = new HttpRequest<any>('GET', 'example.com');
+    fakeCookieService.get.and.returnValue('');
+
+    // act
+    interceptor.intercept(request, fakeHttpHandler)
+
+    // assert
+    const newRequest = fakeHttpHandler.handle.calls.argsFor(0)[0];
+    expect(newRequest.url).toContain(environment.serverHost);
+    expect(newRequest.headers.get('Authorization')).toBeFalsy();
   });
 });
