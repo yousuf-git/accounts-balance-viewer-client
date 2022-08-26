c97bd5596ba87e140ffdb9b18f8d8db446e84c51
yousuf <mohammed.yousuf@pickme.lk>
2022-08-26 08:11:32 +0530

    add unit tests for auth guard

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,16 +1,66 @@
-import { TestBed } from '@angular/core/testing';
-
-import { AuthGuard } from './auth.guard';
+import {AuthGuard} from './auth.guard';
+import {AuthService} from "../services/auth.service";
+import {ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
+import {UserRole} from "../constants";
 
 describe('AuthGuard', () => {
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeActivatedRouteSnapshot: jasmine.SpyObj<ActivatedRouteSnapshot>;
+  let fakeRouterStateSnapshot: jasmine.SpyObj<RouterStateSnapshot>;
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
+    fakeAuthService = jasmine.createSpyObj<AuthService>('AuthService', {
+      getUser: undefined
+    });
+
+    fakeActivatedRouteSnapshot = jasmine.createSpyObj<ActivatedRouteSnapshot>('ActivatedRouteSnapshot', ['toString']);
+    fakeRouterStateSnapshot = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', ['toString']);
+
+    guard = new AuthGuard(fakeAuthService);
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
+  it('activates on auth user having permission to the route', () => {
+    // act
+    fakeActivatedRouteSnapshot.data = {
+      roles: <UserRole[]>[
+        UserRole.Admin
+      ]
+    }
+
+    fakeAuthService.getUser.and.returnValue({
+      id: '33',
+      name: 'Max',
+      email: 'max@email.com',
+      username: 'max33',
+      role: UserRole.Admin
+    });
+
+    const result = guard.canActivate(fakeActivatedRouteSnapshot, fakeRouterStateSnapshot) as boolean;
+
+    // assert
+    expect(result).toBe(true);
+  });
+
+  it('not activates on auth user having not permission to the route', () => {
+    // act
+    fakeActivatedRouteSnapshot.data = {
+      roles: <UserRole[]>[
+        UserRole.User
+      ]
+    }
+
+    fakeAuthService.getUser.and.returnValue({
+      id: '33',
+      name: 'Max',
+      email: 'max@email.com',
+      username: 'max33',
+      role: UserRole.Admin
+    });
+
+    const result = guard.canActivate(fakeActivatedRouteSnapshot, fakeRouterStateSnapshot) as boolean;
+
+    // assert
+    expect(result).toBe(false);
   });
 });
